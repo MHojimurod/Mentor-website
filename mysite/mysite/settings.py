4820b92c630ec3a5b02759d85f28142c6d933c42
MHojimurod <cool.hojimurod2001@mail.ru>
2021-07-22 12:51:11 +0500

    fourth-commit

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+=mjk23bni&!wsxt=)uym2&@uhj)c_w!og@1t*$3f++6uqe&sh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
